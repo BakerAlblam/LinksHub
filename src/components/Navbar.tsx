a6c322560ca52455095ef269a67ae1e7d396d4c3
Baker Haider <33936834+BakerAlblam@users.noreply.github.com>
2024-02-20 12:09:04 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,7 +22,7 @@ export function Navbar() {
         className="flex items-center"
         href={isSignedIn ? `/links/${username}` : `/`}
       >
-        <FilmIcon className="h-6 w-6" color="purple" />
+       
         <span className="ml-2 text-lg font-semibold text-purple-800">
           LinkHub
         </span>
@@ -57,31 +57,7 @@ export function Navbar() {
   );
 }
 
-function FilmIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <rect width="18" height="18" x="3" y="3" rx="2" />
-      <path d="M7 3v18" />
-      <path d="M3 7.5h4" />
-      <path d="M3 12h18" />
-      <path d="M3 16.5h4" />
-      <path d="M17 3v18" />
-      <path d="M17 7.5h4" />
-      <path d="M17 16.5h4" />
-    </svg>
-  );
-}
+
 
 function SearchIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
   return (
